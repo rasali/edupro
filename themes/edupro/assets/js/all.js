dd959ed218fbafb9f97bd8d5cb252e7dea74608e
RassulAli <rasul.aliyev94@gmail.com>
2019-05-31 15:17:27 +0400

    very last commits

--- themes/edupro/assets/js/all.js
+++ themes/edupro/assets/js/all.js
@@ -1,76 +1,61 @@
-$( document ).ready(function() {
+$(document).ready(function () {
     $('.owl-forth .owl-carousel').owlCarousel({
-      loop:true,
-      margin:30,
-    //   nav:true,
-      responsive:{
-          0:{
-              items:1
-          },
-          600:{
-              items:2
-          },
-          1000:{
-              items:4
-          }
-      }
+        loop: true,
+        margin: 30,
+        //   nav:true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 4
+            }
+        }
     });
 
     $('.partners .owl-carousel').owlCarousel({
-      loop:true,
-      margin:30,
-      responsive:{
-        0:{
-          items:1
-        },
-        600:{
-          items:2,
-          nav:true
-        },
-        1000:{
-          items:3
+        loop: true,
+        margin: 30,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2,
+                nav: true
+            },
+            1000: {
+                items: 3
+            }
         }
-      }
     });
 
-    $('.owl-2 .owl-carousel').owlCarousel({
-      mouseDrag: ($('.owl-2 .item').length > 2),
-      loop:($('.owl-2 .item').length > 2),
-      margin:0,
-      nav: ($('.owl-2 .item').length > 2),
-      dots: false,
-      responsive:{
-        0:{
-          items:1
-        },
-        600:{
-          items:2,
-          nav:true
-        },
-        1000:{
-          items:2
-        }
-      }
-    });
+
 
     $('.owl-preferences .owl-carousel').owlCarousel({
-      autoplay: ($('.owl-preferences .item').length > 4),
-      mouseDrag: ($('.owl-preferences .item').length > 4),
-      loop: ($('.owl-preferences .item').length > 4),
-      margin:30,
-      nav: ($('.owl-preferences .item').length > 4),
-      dots: false,
-      responsive:{
-          0:{
-              items:1
-          },
-          600:{
-              items:2
-          },
-          1000:{
-              items:4
-          }
-      }
+        autoplay: ($('.owl-preferences .item').length > 4),
+        mouseDrag: ($('.owl-preferences .item').length > 4),
+        loop: ($('.owl-preferences .item').length > 4),
+        margin: 30,
+        nav: false,
+        dots: false,
+        autoplay:true,
+        autoplayTimeout:100,
+        autoplayHoverPause:true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 4
+            }
+        }
     });
 
     // var totalItems = $('.owl-2 .item').length;
@@ -94,19 +79,19 @@ $( document ).ready(function() {
     //   var isLooped = true;
     //   var isNav = true;
     // }
-  });
+});
 $(document).ready(function () {
     $('.owl-2 .item:even').children().addClass('commendation--1')
         .children(":first-child").addClass("commendation__circle--big commendation__circle--big-1")
         .siblings(":nth-child(2)").addClass("commendation__circle--small commendation__circle--small-1")
         .siblings(":nth-child(3)").addClass("commendation__circle--small commendation__circle--small-1_1");
-        
+
     $('.owl-2 .item:odd').children().addClass('commendation--2')
         .children(":first-child").addClass("commendation__circle--small commendation__circle--small-2")
         .siblings(":nth-child(2)").addClass("commendation__circle--small commendation__circle--small-2_2")
-        .siblings(":nth-child(3)").addClass("commendation__circle--big commendation__circle--big-2");;
+        .siblings(":nth-child(3)").addClass("commendation__circle--big commendation__circle--big-2");
+    ;
 });
-
 
 
 $(document).ready(function () {
